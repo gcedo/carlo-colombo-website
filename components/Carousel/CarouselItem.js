b7c420bedc293f3d0170e052330d4e5d22a0b3b8
Edoardo Colombo <edo.gcolombo@gmail.com>
2016-02-05 00:18:38 +0100

    Specified carousel image dimensions.

--- components/Carousel/CarouselItem.js
+++ components/Carousel/CarouselItem.js
@@ -3,7 +3,7 @@ import cx from 'classnames';
 
 const CarouselItem = ({ imgUri, title, caption, active }) => (
   <div className={cx({ active, item: true })}>
-    <img src={imgUri} alt="" />
+    <img src={imgUri} alt="" width="940px" height="627" />
     <div className="carousel-caption">
       <h3>{title}</h3>
       {<p>{caption}</p>}
